1f2c6d0adb06ebb8171182563de61521db843df4
Galapidok <89271735+Galapidok@users.noreply.github.com>
2021-09-13 09:55:09 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import {
   PerspectiveCamera,
   Scene,
   WebGLRenderer,
-} from 'three';
+} from 'https://cdn.skypack.dev/three@0.132.2';
 
 // Get a reference to the container element that will hold our scene
 const container = document.querySelector('#scene-container');
@@ -55,4 +55,3 @@ container.append(renderer.domElement);
 
 // render, or 'create a still image', of the scene
 renderer.render(scene, camera);
-
