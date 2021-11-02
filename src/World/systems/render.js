234829cdd472a4efeb5bdc380c10bc170d53642e
Galapidok <89271735+Galapidok@users.noreply.github.com>
2021-11-02 13:40:24 -0400

    Delete src directory

--- src/World/systems/render.js
+++
@@ -1,9 +0,0 @@
-import { WebGLRenderer } from 'https://cdn.skypack.dev/three@0.132.2';
-
-function createRenderer() {
-  const renderer = new WebGLRenderer();
-
-  return renderer;
-}
-
-export { createRenderer };
