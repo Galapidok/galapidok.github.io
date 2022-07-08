0493472fbe41c866bb28f3cf521c127e8887b339
Galapidok <89271735+Galapidok@users.noreply.github.com>
2022-07-08 16:09:09 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -44,38 +44,38 @@ function init() {
     renderer.setSize( window.innerWidth, window.innerHeight );
     document.body.appendChild( renderer.domElement );
   
-keys = {
-    a: false,
-    s: false,
-    d: false,
-    w: false
-  };
+    keys = {
+        a: false,
+        s: false,
+        d: false,
+        w: false
+    };
 
-  let button = document.querySelector('#button');
-  let log = document.querySelector('#log');
-  button.addEventListener('mouseup', logMouseButton);
+    let button = document.querySelector('#button');
+    let log = document.querySelector('#log');
+    button.addEventListener('mouseup', logMouseButton);
 
-  function logMouseButton(e) {
-   if (typeof e === 'object') {
-      switch (e.button) {
-        case 0:
-          log.textContent = 'Left button clicked.';
-         break;
-        case 1:
-          log.textContent = 'Middle button clicked.';
-          break;
-        case 2:
-          log.textContent = 'Right button clicked.';
-          break;
-        default:
-          log.textContent = `Unknown button code: ${e.button}`;
+    function logMouseButton(e) {
+     if (typeof e === 'object') {
+        switch (e.button) {
+          case 0:
+            log.textContent = 'Left button clicked.';
+           break;
+          case 1:
+            log.textContent = 'Middle button clicked.';
+            break;
+          case 2:
+            log.textContent = 'Right button clicked.';
+            break;
+          default:
+            log.textContent = `Unknown button code: ${e.button}`;
+      }
     }
   }
-}
 
-  document.body.addEventListener( 'keydown', function(e) {
+   document.body.addEventListener( 'keydown', function(e) {
     
-    var key = e.code.replace('Key', '').toLowerCase();
+   var key = e.code.replace('Key', '').toLowerCase();
     if ( keys[ key ] !== undefined )
       keys[ key ] = true;
     
